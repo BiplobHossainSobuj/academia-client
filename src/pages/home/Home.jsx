05a23ef8ebd442083d0a8c614b5381fa6a8559fa
BiploHossainSobuj <bhs.cse@outlook.com>
2024-05-14 09:51:14 +0600

    added search functionality

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,19 +6,40 @@ import ServiceCard from "../../components/serviceCard/ServiceCard";
 
 const Home = () => {
     const loadedServices = useLoaderData();
-    const [services,setServices] = useState(loadedServices);
-    
+    const [services, setServices] = useState(loadedServices);
+    const [searchKey, setSearchKey] = useState('');
+    let filtered = [];
+    const handleChange = () => {
+        if (searchKey != "") {
+            services.filter(service => {
+                if (service.serviceName.toLocaleLowerCase().includes(searchKey.toLocaleLowerCase())) {
+                    filtered.push(service);
+                }
+            })
+        }
+        return filtered;
+    }
+    // console.log(handleChange())
+    handleChange();
     return (
         <div className='max-w-7xl mx-auto my-24'>
             <h1 className='text-5xl text-primary'>this is home</h1>
             <Banner></Banner>
-                <h1>Popular Services</h1>
+            <h1>Popular Services</h1>
+            <div className=' bg-red-400 p-4 rounded-lg'>
+                <label className="input input-bordered flex items-center gap-2">
+                    <input onChange={(e) => setSearchKey(e.target.value)} type="text" className="grow" placeholder="Search" />
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                </label>
+            </div>
             <div className='grid grid-cols-1 md:grid-cols-2'>
-                {services.map(service=><ServiceCard key={service._id} service={service}></ServiceCard>)}
+                {searchKey ?
+                    filtered.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
+                    :services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)}
             </div>
             <div className="flex justify-center my-6">
 
-            <Link to={`/services`}><button className="btn btn-outline btn-warning">Show More</button></Link>
+                <Link to={`/services`}><button className="btn btn-outline btn-warning">Show More</button></Link>
             </div>
         </div>
     );
--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -2,20 +2,47 @@ import React, { useEffect, useState } from 'react';
 import ServiceCard from '../../components/serviceCard/ServiceCard';
 
 const Services = () => {
-    const [services,setServices] = useState([]);
-
-    useEffect(()=>{
+    const [services, setServices] = useState([]);
+    const [searchKey, setSearchKey] = useState('');
+    let filtered = [];
+    useEffect(() => {
         fetch('http://localhost:5000/services')
-        .then(res=>res.json())
-        .then(data=>{
-            setServices(data)
-        })
-    },[]);
+            .then(res => res.json())
+            .then(data => {
+                setServices(data)
+            })
+    }, []);
+    const handleChange = () => {
+        if (searchKey != "") {
+            services.filter(service => {
+                if (service.serviceName.toLocaleLowerCase().includes(searchKey.toLocaleLowerCase())) {
+                    filtered.push(service);
+                }
+            })
+        }
+        return filtered;
+    }
+    // console.log(handleChange())
+    handleChange();
+
+
+
+
     return (
         <div className='max-w-7xl mx-auto my-12'>
             <div>All services</div>
+            <div className="flex justify-center my-6">
+                <div className='w-2/3 bg-red-400 p-4 rounded-lg'>
+                    <label className="input input-bordered flex items-center gap-2">
+                        <input onChange={(e) => setSearchKey(e.target.value)} type="text" className="grow" placeholder="Search" />
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                    </label>
+                </div>
+            </div>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-                {services.map(service=><ServiceCard key={service._id} service={service}></ServiceCard>)}
+                {searchKey ?
+                    filtered.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
+                    : services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)}
             </div>
         </div>
     );
